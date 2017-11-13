e57e316f4ed3c35d3b5cf34807a1946296a6894e
Brittany Morris <brittmmendez@gmail.com>
2017-11-13 15:21:41 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -5,7 +5,7 @@ end
 WIN_COMBINATIONS=[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
 
 def won?(board)
-  WIN_COMBINATIONS.detect do|w_combination|
+  WIN_COMBINATIONS.each do|w_combination|
     if position_taken?(board, index) && ((board(w_combination[0])==board(w_combination[1])) && (board(w_combination[1])==board(w_combination[2])))
       true
     else
