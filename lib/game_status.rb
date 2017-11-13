90504ef16a737df0ae3c0ff5bfb4a1334fb0da67
Brittany Morris <brittmmendez@gmail.com>
2017-11-13 17:01:59 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -21,3 +21,14 @@ end
 def over?(board)
   draw?(board) ||won?(board)
 end
+
+def winner(board)
+  if WIN_COMBINATIONS.find do|w_combination|
+    (position_taken?(board, w_combination[1]) && board[w_combination[0]]==board[w_combination[1]] && board[w_combination[1]]==board[w_combination[2]])
+    && board[w_combination[0]]=="X" &&board[w_combination[1]]=="X" &&board[w_combination[2]]=="X"
+    puts "X"
+  elsif WIN_COMBINATIONS.find do|w_combination|
+    (position_taken?(board, w_combination[1]) && board[w_combination[0]]==board[w_combination[1]] && board[w_combination[1]]==board[w_combination[2]])
+    && board[w_combination[0]]=="O" &&board[w_combination[1]]=="O" &&board[w_combination[2]]=="O"
+    puts "O"
+  end
