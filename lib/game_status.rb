6c1460ea99501494bf8a1a305867d313b5fb30f4
Brittany Morris <brittmmendez@gmail.com>
2017-11-13 15:56:26 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -6,11 +6,11 @@ WIN_COMBINATIONS=[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6
 
 def won?(board)
   WIN_COMBINATIONS.find do|w_combination|
-    position_taken?(board, w_combination[1]) && board[w_combination[0]]==board[w_combination[1]] 
+    position_taken?(board, w_combination[1]) && board[w_combination[0]]==board[w_combination[1]]
     && board[w_combination[1]]==board[w_combination[2]]
     end
 end
 
 def full?(board)
-  
+
 end
