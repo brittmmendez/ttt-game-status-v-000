7a3e3e028b513baa857651262a7dd522e81f3837
Brittany Morris <brittmmendez@gmail.com>
2017-11-13 15:36:09 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -5,7 +5,7 @@ end
 WIN_COMBINATIONS=[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
 
 def won?(board)
-  WIN_COMBINATIONS.each do|w_combination|
+  WIN_COMBINATIONS.find do|w_combination|
     (board(w_combination[0])==board(w_combination[1])) && (board(w_combination[1])==board(w_combination[2]))
     end
 end
